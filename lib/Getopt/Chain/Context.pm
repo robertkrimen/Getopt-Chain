9a17ae2787b74430a37bbefea9ff57b6e5b2eef9
robertkrimen <robertkrimen@gmail.com>
2008-08-09 11:12:16 -0700

    Added rudimentary forwarding... ugly hack, though

--- lib/Getopt/Chain/Context.pm
+++ lib/Getopt/Chain/Context.pm
@@ -4,6 +4,7 @@ use strict;
 use warnings;
 
 use Moose;
+use Getopt::Chain::Carp;
 
 has chain => qw/is ro isa ArrayRef/, default => sub { [] };
 
@@ -19,6 +20,32 @@ sub push {
     my $link = Getopt::Chain::Context::Link->new(context => $self, @_);
     push @{ $self->chain }, $link;
     return $link;
+}
+
+sub pop {
+    my $self = shift;
+    pop @{ $self->chain };
+}
+
+sub run {
+    my $self = shift;
+    my $path = shift || "";
+
+    my @path = grep { length $_ } split m/[ \/]+/, $path;
+
+    my $link = $self->link;
+    my $processor = $self->link(0)->processor;
+    for (@path) {
+        # TODO Probably call this 'resolve'
+        $processor = $processor->commands->{$_} or croak "Couldn't traverse $path: $_ not found";
+    }
+
+    $self->push(processor => $processor, command => $path[-1],
+        arguments => $link->_arguments, remaining_arguments => $link->_remaining_arguments, options => $link->options);
+
+    $processor->run->($self, @_);
+
+    $self->pop;
 }
 
 sub update {
